5ac24a7786e5299d71eb3c92c59d8911361ed29a
Alexandre Delétoille <alexandre.deletoille@proton.me>
2025-10-09 14:42:21 +0200

    update dagger

---
+++ .dagger/src/ci.ts
@@ -0,0 +1,119 @@
+import {
+  dag,
+  Container,
+  Directory,
+  object,
+  func,
+  argument,
+  Secret,
+} from "@dagger.io/dagger";
+
+@object()
+export class CgdFrontend {
+  @func()
+  async dockerBuildPublish(
+    @argument({
+      defaultPath: "/",
+      ignore: ["node_modules", "build", ".svelte-kit"],
+    })
+    source: Directory,
+    dockerUsername: string = "",
+    dockerPassword: string = ""
+  ): Promise<string> {
+    /* await this.runTests(source); */
+    /* if (dockerUsername == "" || dockerPassword == "") {
+      dockerUsername = process.env.DOCKER_USERNAME;
+      dockerPassword = process.env.DOCKER_USERNAME;
+    } */
+    let secretDockerPassword: Secret = dag.setSecret(
+      "docker_password",
+      dockerPassword
+    );
+    // Stage 1: Build stage
+    const builder = dag
+      .container()
+      .from("node:23.11.0-slim")
+      .withWorkdir("/app")
+      .withFile("/app/package.json", source.file("package.json"))
+      .withFile("/app/package-lock.json", source.file("package-lock.json"))
+      .withMountedCache("/root/.npm", dag.cacheVolume("node"))
+      .withExec(["npm", "install"])
+      .withDirectory("/app", source)
+      .withExec(["npm", "install", "--save-dev", "@sveltejs/adapter-static"])
+      .withEnvVariable("NODE_ENV", "production")
+      .withExec(["npm", "run", "build"])
+      .directory("./dist");
+
+    // Stage 2: Production stage
+    const build = dag
+      .container()
+      .from("nginx:1.29.2-alpine")
+      .withDirectory("/usr/share/nginx/html", builder)
+      .withFile(
+        "/etc/nginx/conf.d/default.conf",
+        source.file("nginx/nginx.conf")
+      )
+      .withRegistryAuth("docker.io", dockerUsername, secretDockerPassword)
+      .withExposedPort(80);
+    /* .withExec(["nginx", "-g", "daemon off;"]); */
+
+    // Publication sur DockerHub
+    console.log("🚀 Publication on DockerHub...");
+    const imageRef = `${dockerUsername}/cgd-frontend:latest`;
+    try {
+      await build.publish(imageRef);
+      return `✅ Image successfully published : ${imageRef}`;
+    } catch (error) {
+      console.error(`❌ Error during publication: ${error}`);
+      throw error;
+    }
+  }
+
+  /**
+   * Return the result of running unit tests
+   */
+  @func()
+  async runTests(
+    @argument({ defaultPath: "/" }) source: Directory
+  ): Promise<string> {
+    try {
+      return this.buildEnvTests(source)
+        .withExec(["npm", "run", "build"])
+        .withExec(["npx", "playwright", "test"])
+        .stdout();
+    } catch (error) {
+      console.error(`❌ Error during tests execution: ${error}`);
+      throw error;
+    }
+  }
+
+  /**
+   * Build a ready-to-use development environment
+   */
+  @func()
+  buildEnvTests(@argument({ defaultPath: "/" }) source: Directory): Container {
+    try {
+      const nodeCache = dag.cacheVolume("node");
+      return (
+        dag
+          .container()
+          .from("node:23.11.0-slim")
+          .withDirectory("/src", source)
+          .withMountedCache("/root/.npm", nodeCache)
+          .withWorkdir("/src")
+          .withExec(["npm", "install"])
+          // Install Playwright browsers and browser system dependencies
+          .withExec([
+            "npx",
+            "-y",
+            "playwright@latest",
+            "install",
+            "--with-deps",
+          ])
+      );
+    } catch (error) {
+      console.error(`❌ Error during build environment tests: ${error}`);
+      throw error;
+    }
+  }
+}
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM node:slim AS builder
+# Stage 1: Build stage
+FROM node:23.11.0-slim AS builder
 
 WORKDIR /app
 
@@ -17,8 +18,8 @@ ENV NODE_ENV=production
 # Build de l'application
 RUN npm run build
 
-# Production stage
-FROM nginx:alpine
+# Stage 2: Production stage
+FROM nginx:1.29.2-alpine
 
 # Copie des fichiers buildés directement dans le répertoire html de nginx
 COPY --from=builder /app/build /usr/share/nginx/html
